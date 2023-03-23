6832ee2ddd72d2f19a98c09d22742cea397cc69e
besser <besser9009@mail.ru>
2023-03-23 17:12:08 +0300

    add Example005 Conditions if-else

---
+++ Example005_Condindition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("О, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
